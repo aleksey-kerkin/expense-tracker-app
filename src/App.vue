94156ce73eef5eaaa6631d6171d2f019884386b3
Aleksey Kerkin <aleksey.kerkin@yandex.ru>
2025-07-02 02:32:39 +0500

    Add toast when adding a transaction

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup>
 import { ref, computed } from 'vue';
+import { useToast } from 'vue-toastification';
 
 import Header from './components/Header.vue';
 import Balance from './components/Balance.vue';
 import IncomeExpenses from './components/IncomeExpenses.vue';
 import TransactionList from './components/TransactionList.vue';
 import AddTransaction from './components/AddTransaction.vue';
+
+const toast = useToast();
 
 const transactions = ref([
   { id: 1, text: 'Flower', amount: -19.99 },
@@ -45,6 +48,8 @@ const handleTransactionSubmitted = (transactionData) => {
     text: transactionData.text,
     amount: transactionData.amount,
   });
+
+  toast.success('Transaction added')
 };
 
 const generateUniqueId = () => {
